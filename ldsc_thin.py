907f46ad4692d5466a4452a45e9b8a5bba9f477a
YiliangTracyZhang <yiliang.zhang@yale.edu>
2021-10-19 00:31:42 -0400

    ldscore

--- ldsc_thin.py
+++ ldsc_thin.py
@@ -1,73 +1,10 @@
 #!/usr/bin/env python
-'''
-(c) 2014 Brendan Bulik-Sullivan and Hilary Finucane
 
-LDSC is a command line tool for estimating
-    1. LD Score
-    2. heritability / partitioned heritability
-    3. genetic covariance / correlation
-
-'''
 from __future__ import division, print_function
-import ldscore.ldscore as ld
-import ldscore.parse as ps
+import ld.ldscore as ld
+import ld.parse as ps
 import numpy as np
 import pandas as pd
-
-try:
-    x = pd.DataFrame({'A': [1, 2, 3]})
-    x.drop_duplicates(subset='A')
-except TypeError:
-    raise ImportError('LDSC requires pandas version > 0.15.2')
-
-__version__ = '1.0.0'
-MASTHEAD = "*********************************************************************\n"
-MASTHEAD += "* LD Score Regression (LDSC)\n"
-MASTHEAD += "* Version {V}\n".format(V=__version__)
-MASTHEAD += "* (C) 2014-2015 Brendan Bulik-Sullivan and Hilary Finucane\n"
-MASTHEAD += "* Broad Institute of MIT and Harvard / MIT Department of Mathematics\n"
-MASTHEAD += "* GNU General Public License v3\n"
-MASTHEAD += "*********************************************************************\n"
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
-pd.set_option('precision', 4)
-pd.set_option('max_colwidth',1000)
-np.set_printoptions(linewidth=1000)
-np.set_printoptions(precision=4)
-
-
-def sec_to_str(t):
-    '''Convert seconds to days:hours:minutes:seconds'''
-    [d, h, m, s, n] = reduce(lambda ll, b : divmod(ll[0], b) + ll[1:], [(t, 1), 60, 60, 24])
-    f = ''
-    if d > 0:
-        f += '{D}d:'.format(D=d)
-    if h > 0:
-        f += '{H}h:'.format(H=h)
-    if m > 0:
-        f += '{M}m:'.format(M=m)
-
-    f += '{S}s'.format(S=s)
-    return f
-
-
-class Logger(object):
-    '''
-    Lightweight logging.
-    TODO: replace with logging module
-
-    '''
-    def __init__(self, fh):
-        self.log_fh = open(fh, 'wb')
-
-    def log(self, msg):
-        '''
-        Print to log file.
-        TODO: Also print to stdout if verbose flag is set.
-
-        '''
-        print(msg, file=self.log_fh)
 
 
 def _remove_dtype(x):
@@ -85,19 +22,17 @@ def loj_bim(filter_df, array):
     merge_df.loc[:,'keep'] = True
     z = pd.merge(array.IDList, merge_df, how='left', left_on=l, right_on=r, sort=False)
     ii = z['keep'] == True
-    return np.nonzero(ii)[0]
+    return ii.to_numpy().nonzero()[0]
 
 
-def __filter_bim__(filter_df, array, log):
+def __filter_bim__(filter_df, array):
     merged_list = loj_bim(filter_df, array)
     len_merged_list = len(merged_list)
     if len_merged_list > 0:
         c = 'After merging, {0} SNPs remain'
-        log.log(c.format(len_merged_list))
     else:
         error_msg = 'No SNPs retained for analysis'
         raise ValueError(error_msg)
-
     return merged_list
 
 
@@ -115,7 +50,7 @@ def remove_brackets(x):
     return x.replace('[', '').replace(']', '').strip()
 
 
-def _ldscore(bfile, annots, gwas_snps):
+def _ldscore(bfile, gwas_snps):
     '''
     Wrapper function for estimating l1, l1^2, l2 and l4 (+ optionally standard errors) from
     reference panel genotypes.
@@ -124,7 +59,6 @@ def _ldscore(bfile, annots, gwas_snps):
     chr snp bp cm <annotations>
 
     '''
-    log = Logger('ldsc.log')
 
     snp_file, snp_obj = bfile+'.bim', ps.PlinkBIMFile
     ind_file, ind_obj = bfile+'.fam', ps.PlinkFAMFile
@@ -133,38 +67,20 @@ def _ldscore(bfile, annots, gwas_snps):
     array_snps = snp_obj(snp_file)
     # snp list
     m = len(array_snps.IDList)
-    log.log('Read list of {m} SNPs from {f}'.format(m=m, f=snp_file))
-    if annots is not None:  # read --annot
-        try:
-            annot = ps.AnnotFile(pd.concat(annots, ignore_index=True))
-            n_annot, ma = len(annot.df.columns) - 4, len(annot.df)
-            log.log("Read {A} annotations for {M} SNPs".format(A=n_annot, M=ma))
-            annot_colnames = annot.df.columns[4:]
-            keep_snps = None
-            #take only annot SNPs in intersect
-            kept_cols = len(annot.df.columns)
-            annot.df = subset_annot_file(annot.df, gwas_snps, kept_cols)
-            annot_matrix = np.array(annot.df.iloc[:,4:])
-        except Exception:
-            log.log('Error parsing .annot file')
-            raise
-    else:
-        annot_matrix, annot_colnames, keep_snps = None, None, None,
-        n_annot = 1
+    annot_matrix, annot_colnames, keep_snps = None, None, None,
+    n_annot = 1
 
-    keep_snps = __filter_bim__(gwas_snps, array_snps, log)
+    keep_snps = __filter_bim__(gwas_snps, array_snps)
 
 
     # read fam
     array_indivs = ind_obj(ind_file)
     n = len(array_indivs.IDList)
-    log.log('Read list of {n} individuals from {f}'.format(n=n, f=ind_file))
     # read keep_indivs
     keep_indivs = None
 
     # read genotype array
-    log.log('Reading genotypes from {fname}'.format(fname=array_file))
-    geno_array = array_obj(array_file, n, array_snps, log, keep_snps=keep_snps,
+    geno_array = array_obj(array_file, n, array_snps, keep_snps=keep_snps,
         keep_indivs=keep_indivs, mafMin=None)
 
     #determine block widths
@@ -177,12 +93,9 @@ def _ldscore(bfile, annots, gwas_snps):
     scale_suffix = ''
 
     lN = geno_array.ldScoreVarBlocks(block_left, 50, annot=annot_matrix)
-    col_prefix = "L2";
-
-    if n_annot == 1:
-        ldscore_colnames = [col_prefix+scale_suffix]
-    else:
-        ldscore_colnames =  [y+col_prefix+scale_suffix for y in annot_colnames]
+    col_prefix = "L2"
+        
+    ldscore_colnames = [col_prefix+scale_suffix]
 
     # print .ldscore. Output columns: CHR, BP, RS, [LD Scores]
     new_colnames = geno_array.colnames + ldscore_colnames
@@ -192,66 +105,27 @@ def _ldscore(bfile, annots, gwas_snps):
 
     # print LD Score summary
     pd.set_option('display.max_rows', 200)
-    log.log('\nSummary of LD Scores')
-    t = df.ix[:,4:].describe()
-    log.log( t.ix[1:,:] )
+    t = df.iloc[:,4:].describe()
 
     np.seterr(divide='ignore', invalid='ignore')  # print NaN instead of weird errors
     # print correlation matrix including all LD Scores and sample MAF
-    log.log('')
-    log.log('MAF/LD Score Correlation Matrix')
-    log.log( df.ix[:,4:].corr() )
-
-    # print condition number
-    if n_annot > 1: # condition number of a column vector w/ nonzero var is trivially one
-        log.log('\nLD Score Matrix Condition Number')
-        cond_num = np.linalg.cond(df.ix[:,5:])
-        log.log(remove_brackets(str(np.matrix(cond_num))))
-        if cond_num > 10000:
-            log.log('WARNING: ill-conditioned LD Score Matrix!')
-
-    # summarize annot matrix if there is one
-    if annot_matrix is not None:
-        # covariance matrix
-        x = pd.DataFrame(annot_matrix, columns=annot_colnames)
-        log.log('\nAnnotation Correlation Matrix')
-        log.log( x.corr() )
-
-        # column sums
-        log.log('\nAnnotation Matrix Column Sums')
-        log.log(_remove_dtype(x.sum(axis=0)))
-
-        # row sums
-        log.log('\nSummary of Annotation Matrix Row Sums')
-        row_sums = x.sum(axis=1).describe()
-        log.log(_remove_dtype(row_sums))
 
     np.seterr(divide='raise', invalid='raise')
     return df
 
 
-def ldscore(bfile, annots, gwas_snps, save_ld):
+def ldscore(bfile, gwas_snps):
     df = None
     if '@' in bfile:
         all_dfs = []
         for i in range(1, 23):
             cur_bfile = bfile.replace('@', str(i))
-            if annots is None:
-                cur_annot = None
-            elif len(annots) > 1:
-                cur_annot = [annots[i - 1]]
-            else:
-                cur_annot = annots
-            all_dfs.append(_ldscore(cur_bfile, cur_annot, gwas_snps))
+            all_dfs.append(_ldscore(cur_bfile, gwas_snps))
             print('Computed LD scores for chromosome {}'.format(i))
         df = pd.concat(all_dfs)
     else:
-        df = _ldscore(bfile, annots, gwas_snps)
+        df = _ldscore(bfile, gwas_snps)
 
     numeric = df._get_numeric_data()
     numeric[numeric < 0] = 0
-    if save_ld is not None:
-        file_name = save_ld + '.csv.gz'
-        print('Saving computed LD scores to {}.'.format(file_name))
-        df.to_csv(file_name, ' ', index=False, compression='gzip')
     return df
